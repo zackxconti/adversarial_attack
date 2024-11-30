03703266f726e76e4d7c38035b1c5f0ab5d8b672
zxuerebconti <zxuerebconti@turing.ac.uk>
2024-11-30 14:33:24 +0000

    add: function call for plotting adversarial and original image

--- adversarial_attack/main.py
+++ adversarial_attack/main.py
@@ -22,26 +22,13 @@ def main ():
     initial_prediction(model, original_image_batch,labels)
 
     # generate adversarial noise for target class of choice and validate
-    target_class = 189 
+    target_class = 189 # Lakeland terrier
     adversarial_image = generate_adversarial_noise(model, original_image_batch, target_class)
     validate_model(model, adversarial_image, labels)  
 
-    # print ("predicted class = " ,labels[predicted_class])
+    # plot original and targeted-adversarial image
+    visualise(original_image, adversarial_image)
 
-    # # visualize the original and adversarial images
-    # original_np = denorm_image(image_batch, device)
-    # adversarial_np = denorm_image(adversarial_image, device)
-    # plt.figure(figsize=(10, 5))
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(original_np)
-    # plt.title("Original Image")
-    # plt.axis("off")
-
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(adversarial_np)
-    # plt.title("Adversarial Image")
-    # plt.axis("off")
-    # plt.show()
 
     
 if __name__ == "__main__":
